252b912e0ef5abba323d8f61178e2716f2a53141
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-27 21:43:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,3 @@
 class Song
   attr_accessor :title, :artist
-  
-  def initialize(title)
-    @title = title
-  end
 end
-
-eminem = Artist.new("Eminem")
-lose_yourself = Song.new("Lose Yourself")
-lose_yourself.artist = eminem
